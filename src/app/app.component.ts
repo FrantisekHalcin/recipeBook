8cbeeb026660e0f2663b86ca9af74b6b1ad2818f
Frantisek Halcin <halcin.ferri@gmail.com>
2023-04-28 14:33:29 +0200

    Commit 3, prepinanie medzi 2 sekciami

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  activeSection: string = "recipe"
+
+  switch(data) {
+    this.activeSection = data;
+  }
 }
