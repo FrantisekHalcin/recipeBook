a4bdf7f5276c685fb56368aff6091a8049686b64
Frantisek Halcin <halcin.ferri@gmail.com>
2023-05-13 13:09:55 +0200

    Commit 17 dynamic component

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,18 +1,23 @@
-import {Component} from '@angular/core';
+import {Component, OnDestroy, ViewChild} from '@angular/core';
 import {NgForm} from "@angular/forms";
 import {AuthResponseData, AuthService} from "./auth.service";
-import {Observable} from "rxjs";
+import {Observable, Subscription} from "rxjs";
 import {Router} from "@angular/router";
+import { AlertComponent } from "../shared/alert/alert.component";
+import {ViewContainerRef} from "@angular/core";
 
 @Component({
     selector: 'app-auth',
     templateUrl: './auth.component.html',
     styleUrls: ['./auth.component.css']
 })
-export class AuthComponent {
+export class AuthComponent implements OnDestroy {
     logMode = true;
     isLoading: boolean = false;
     error: string = null;
+    mySub: Subscription;
+
+    @ViewChild('tutodaj', { read: ViewContainerRef }) tuRef: ViewContainerRef;
 
     constructor(
         private as: AuthService,
@@ -50,9 +55,31 @@ export class AuthComponent {
                 this.isLoading = false
 
                 this.error = errorMes;
+                this.showError(errorMes)
             }
         });
         form.reset();
+    }
+
+    closeModal() {
+        this.error = null;
+    }
+
+    showError (mess: string) {
+        const componentRef = this.tuRef.createComponent(AlertComponent);
+        componentRef.instance.mess = mess;
+        this.mySub = componentRef.instance.close.subscribe(()=>{
+            this.tuRef.clear();
+            this.mySub.unsubscribe();
+
+        })
+    }
+
+    ngOnDestroy() {
+        if (this.mySub){
+            this.mySub.unsubscribe();
+        }
+
     }
 
 }
