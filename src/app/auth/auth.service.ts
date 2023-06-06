55ff0847e62b79531d02648f04b89129b84c3b4c
Frantisek Halcin <halcin.ferri@gmail.com>
2023-06-06 10:27:09 +0200

    Commit 23 ShoppingList and Auth migrated to NgRx

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,15 +2,6 @@ import {Injectable} from '@angular/core';
 import {Store} from "@ngrx/store";
 import * as fromAuthActions from './store/auth.actions'
 
-export interface AuthResponseData {
-    idToken: string;
-    email: string;
-    refreshToken: string;
-    expiresIn: string;
-    localId: string;
-    registered?: boolean;
-}
-
 @Injectable({
     providedIn: 'root'
 })
